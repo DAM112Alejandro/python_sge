84d420f94696d1dff4dfc1cc656a0fe316f504a6
arodriguez <arodriguez@hiberus.com>
2023-01-19 11:49:32 +0100

    cest fini

--- SGE/funciones/ej2.py
+++ SGE/funciones/ej2.py
@@ -1,14 +1,15 @@
 from random import randint, uniform,random
 
 def func(numero):
-    valor=int(input("Introduce un valor entre 1 y 1000: "))
+    valor=int(input("Introduce un valor entre 1 y 10: "))
     while valor!=numero:
         if valor>numero:
             print(str(valor)+" es mayor que el que buscas")
             valor=int(input("Introduce un valor entre 1 y 10: "))
-        else :
+        else: 
             print(str(valor)+" es menor que el que buscas")
             valor=int(input("Introduce un valor entre 1 y 10: "))
+    print("Has acertado")
+        
       
-func(randint(0,1000))
-print("Has acertado")
+func(randint(0,10))
